250edd6ce15c38705bc97c81af1c4d1b0fef1d20
Pablo <pablo0657@hotmail.com>
2022-08-20 14:15:32 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e65a1367.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
